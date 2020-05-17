14d5e9137b0ad69742d2f2e18d90747c782d0768
vquilon <v.quilonr@gmail.com>
2020-05-17 12:23:57 +0200

    Funcionalidad con Firebase

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.14.4/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCmThpeVYIkHm8Le6p7TIIZqM1SLpiESbM",
+    authDomain: "import-your-ideas.firebaseapp.com",
+    databaseURL: "https://import-your-ideas.firebaseio.com",
+    projectId: "import-your-ideas",
+    storageBucket: "import-your-ideas.appspot.com",
+    messagingSenderId: "666231897546",
+    appId: "1:666231897546:web:d1930d9c707ce32635e333",
+    measurementId: "G-2MP4LX0KVD"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
